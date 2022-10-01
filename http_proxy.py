cd982d61823fc084e8c34e2aeefecd0a59376f7d
Mathemagician667 <chiniwalas@gmail.com>
2022-09-30 21:27:38 -0500

    Testing - Sahil

--- http_proxy.py
+++ http_proxy.py
@@ -95,7 +95,7 @@ def proxy(client_socket,client_IP):
     pass
 
 
-def main():
+def main(): # this is the main function
     # check arguments
     if(len(sys.argv)!=2 and len(sys.argv)!=3): # Error Checking
         print("Incorrect number of arguments. \nUsage python3 http_proxy.py PORT")
